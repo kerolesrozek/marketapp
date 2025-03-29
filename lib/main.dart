417ed75a03c52d19bce2aff069c4a59832f2e607
keroles Rozek <kerolesrozek2@gmail>
2025-03-29 14:40:24 +0200

    add firebase auth package and fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fruitesapp/core/app_routes.dart';
 import 'package:fruitesapp/core/services/shared_prefrence_singletone.dart';
+import 'package:fruitesapp/firebase_options.dart';
 
 import 'generated/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(FruitsMarket());
 }
